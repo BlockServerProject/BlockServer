611e14cedd1e7c20b1186c0b21fe40beb79edac2
Falkirks <Falkirks@users.noreply.github.com>
2014-05-29 11:06:50 -0700

    Create Player.java

---
+++ src/blockserver/Player.java
@@ -0,0 +1,21 @@
+package blockserver;
+
+public class Player{
+  public String ip;
+  public int port;
+  public String username;
+  public int eid;
+  public long cid;
+  public float x;
+  public float y;
+  public float z;
+  public float yaw;
+  public float pitch;
+  
+  public Player(String i, int p, int e, int c){
+    ip = i;
+    port = p;
+    eid = e;
+    cid = c;
+  }
+}
