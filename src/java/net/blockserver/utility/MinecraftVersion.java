1ccba93759fceb47150f2dfa70a3dbda26175805
jython234 <jython234@hotmail.com>
2014-07-23 21:38:55 -0500

    Fixed last commit.

---
+++ src/java/net/blockserver/utility/MinecraftVersion.java
@@ -0,0 +1,30 @@
+package net.blockserver.utility;
+
+public enum MinecraftVersion {
+	V081, V090, V091, V092, V093, V094;
+
+	public static String versionToString(MinecraftVersion version){
+		if(version == V081){
+			return "0.8.1";
+		}
+		else if(version == V090){
+			return "0.9.0";
+		}
+		else if(version == V091){
+			return "0.9.1";
+		}
+		else if(version == V092){
+			return "0.9.2";
+		}
+		else if(version == V093){
+			return "0.9.3";
+		}
+		else if(version == V094){
+			return "0.9.4";
+		}
+		else{
+			return null;
+		}
+	}
+
+}
