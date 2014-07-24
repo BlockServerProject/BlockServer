afc1258392529753e71ea32e9ed2407f119a77c5
jython234 <jython234@users.noreply.github.com>
2014-07-23 21:18:27 -0500

    Added more versions.

--- src/java/net/blockserver/utility/MinecraftVersion.java
+++ src/java/net/blockserver/utility/MinecraftVersion.java
@@ -1,7 +1,7 @@
 package net.blockserver.utility;
 
 public enum MinecraftVersion {
-	V081, V090, V091;
+	V081, V090, V091, V092, V093, V094;
 	
 	public static String versionToString(MinecraftVersion version){
 		if(version == V081){
@@ -12,6 +12,15 @@ public enum MinecraftVersion {
 		}
 		else if(version == V091){
 			return "0.9.1";
+		}
+		else if(version == V092){
+			return "0.9.2";
+		}
+		else if(version == V093){
+			return "0.9.3";
+		}
+		else if(version == V094){
+			return "0.9.4";
 		}
 		else{
 			return null;
