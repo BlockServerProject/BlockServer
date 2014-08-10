d99086f2e54b86cad19246b1b47531fbd239c1f5
MalekAlrwily <malek.alrwily@gmail.com>
2014-08-11 01:48:58 +0300

    Improve generation stuff

--- src/java/net/blockserver/level/generator/Generator.java
+++ src/java/net/blockserver/level/generator/Generator.java
@@ -1,5 +1,20 @@
 package net.blockserver.level.generator;
 
-public abstract class Generator {
-    
+import java.util.Collections;
+import java.util.Set;
+
+public abstract class Generator<T extends Enum & GenerationSettings> {
+
+    protected Set<T> customSettings;
+    protected Set<DefaultGenerationSettings> defaultGenerationSettings;
+
+    public void generate(Set<DefaultGenerationSettings> defaultGenerationSettings){
+        this.generate(defaultGenerationSettings, Collections.<T>emptySet());
+    }
+
+    public void generate(Set<DefaultGenerationSettings> defaultGenerationSettings, Set<T> customSettings){
+        this.defaultGenerationSettings = defaultGenerationSettings;
+        this.customSettings = customSettings;
+    }
+
 }
