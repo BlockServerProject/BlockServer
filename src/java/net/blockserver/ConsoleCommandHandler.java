c662d44c837b4003ab29b940612ebc1c1e3a3b67
PEMapModder <pemapmodder1970@gmail.com>
2014-08-19 16:17:06 +0800

    Commands work

--- src/java/net/blockserver/ConsoleCommandHandler.java
+++ src/java/net/blockserver/ConsoleCommandHandler.java
@@ -1,12 +1,12 @@
 package net.blockserver;
 
-import net.blockserver.utility.Command;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ConsoleCommandHandler extends Thread {
+import net.blockserver.utility.CommandIssuer;
+
+public class ConsoleCommandHandler extends Thread implements CommandIssuer{
 	private Server server;
 	protected boolean running;
 	
@@ -23,33 +23,34 @@ public class ConsoleCommandHandler extends Thread {
 		while(this.running){
 			try {
 				String input = console.readLine();
-				if(input.startsWith(Command.getCommandName(Command.FREE))){
-					long total = Runtime.getRuntime().totalMemory() / 1000000;
-					long free = Runtime.getRuntime().freeMemory() / 1000000;
-					long used = total - free;
-					
-					System.out.println("Allocated memory: "+total + " mb");
-					System.out.println("Free memory: "+free + " mb");
-					System.out.println("Memory in use: "+used + " mb");
-				}
-				else if(input.startsWith(Command.getCommandName(Command.STOP))){
-					try {
-						System.exit(0);
-					} catch (Exception e) {
-						server.getLogger().info("Failed to stop server: ");
-						e.printStackTrace();
-					}
-					
-				}
-				else if(input.startsWith(Command.getCommandName(Command.HELP))){
-					System.out.println("- Showing help page 1 of 1 (/help <pageNumber>) -");
-					System.out.println("/free: Shows the available space of memory.");
-					System.out.println("/help: Shows available commands.");
-					System.out.println("/stop: Stops the server.");
-				}
-				else{
-					server.getLogger().warning("Unkown command: "+input);
-				}
+				sudoCommand(input.trim());
+//				if(input.startsWith(Command.getCommandName(Command.FREE))){
+//					long total = Runtime.getRuntime().totalMemory() / 1000000;
+//					long free = Runtime.getRuntime().freeMemory() / 1000000;
+//					long used = total - free;
+//					
+//					System.out.println("Allocated memory: "+total + " mb");
+//					System.out.println("Free memory: "+free + " mb");
+//					System.out.println("Memory in use: "+used + " mb");
+//				}
+//				else if(input.startsWith(Command.getCommandName(Command.STOP))){
+//					try {
+//						System.exit(0);
+//					} catch (Exception e) {
+//						server.getLogger().info("Failed to stop server: ");
+//						e.printStackTrace();
+//					}
+//					
+//				}
+//				else if(input.startsWith(Command.getCommandName(Command.HELP))){
+//					System.out.println("- Showing help page 1 of 1 (/help <pageNumber>) -");
+//					System.out.println("/free: Shows the available space of memory.");
+//					System.out.println("/help: Shows available commands.");
+//					System.out.println("/stop: Stops the server.");
+//				}
+//				else{
+//					server.getLogger().warning("Unknown command: "+input);
+//				}
 			} catch (IOException e) {
 				this.server.getLogger().error("IOException in ConsoleCommand Thread:");
 				e.printStackTrace();
@@ -65,5 +66,15 @@ public class ConsoleCommandHandler extends Thread {
 	public void Stop(){
 		this.running = false;
 	}
+
+    @Override
+    public void sendMessage(String msg) {
+        server.getLogger().info(msg);
+    }
+
+    @Override
+    public void sudoCommand(String line) {
+        // TODO Auto-generated method stub
+    }
 
 }
--- src/java/net/blockserver/utility/Command.java
+++ src/java/net/blockserver/utility/Command.java
@@ -1,46 +1,6 @@
 package net.blockserver.utility;
 
-public enum Command {
-	FREE(0),
-	STOP(1),
-	HELP(2);
-	
-	private int id;
-	
-	private Command(int cmdId){
-		this.id = cmdId;
-	}
-	
-	public static Command getCommandByName(String name){
-		if(name.equalsIgnoreCase("free")){
-			return Command.FREE;
-		}
-		else if(name.equalsIgnoreCase("stop")){
-			return Command.STOP;
-		}
-		else if(name.equalsIgnoreCase("help")){
-			return Command.HELP;
-		}
-		else{
-			return null;
-		}
-	}
-	
-	public static String getCommandName(Command cmd){
-		if(cmd == FREE){
-			return "free";
-		}
-		else if(cmd == STOP){
-			return "stop";
-		}
-		else if(cmd == HELP){
-			return "help";
-		}
-		else{
-			return null;
-		}
-	}
-	
-	
-
+public abstract class Command {
+	public abstract String getName();
+	public abstract void run(CommandIssuer issuer, String[] args);
 }
