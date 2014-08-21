7d95ffa9636fb0b2de51c221635850655fa5e306
Jose Vazquez <jose-vm@users.noreply.github.com>
2014-08-20 22:15:23 -0500

    Update EntityType.java in @PEMapModder 's way.

--- src/java/net/blockserver/entity/EntityType.java
+++ src/java/net/blockserver/entity/EntityType.java
@@ -4,7 +4,22 @@ import net.blockserver.player.Player;
 
 public enum EntityType {
 	CHICKEN(10, Chicken.class),
-	PLAYER(63, Player.class); // TODO more entities
+	COW(11, Cow.class),
+	PIG(12, Pig.class),
+	SHEEP(13, Sheep.class),
+	WOLF(14, Wolf.class),
+	VILLAGER(15, Villager.class),
+	MOOSHROOM(16, Mooshroom.class),
+	ZOMBIE(32, Zombie.class),
+	CREEPER(33, Creeper.class),
+	SKELETON(34, Skeleton.class),
+	SPIDER(35, Spider.class),
+	ZOMBIE_PIG(36, ZombiePig.class),
+	SLIME(37, Slime.class),
+	ENDERMAN(38, Enderman.class),
+	SILVERFISH(39, Silverfish.class),
+	PLAYER(63, Player.class), // TODO more entities
+	//FALLING_BLOCK(66, FallingBlock.class);
 	private int id;
 	private Class<? extends Entity> c;
 	private EntityType(int id, Class<? extends Entity> c){
