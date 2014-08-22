8dc9ac7aaad444fbb8910dd2e5b78ec2b2832d7a
PEMapModder <pemapmodder1970@gmail.com>
2014-08-22 19:34:01 +0800

    Added pitch neutralizer so that mobs no longer fly when they look up

--- src/java/net/blockserver/entity/Living.java
+++ src/java/net/blockserver/entity/Living.java
@@ -5,21 +5,30 @@ import net.blockserver.math.Vector3d;
 import net.blockserver.math.YPSControlledVector3d;
 
 /**
- * Class Living extends Entity
+ * <p>This abstract superclass is the implementation of AI for entities that have brains.</p>
  *
- * This abstract superclass is the implementation of AI for entities that have brains.
- *
- * Definition of "have brains":
- * * Would wander around
- * * Would look for targets
- * * Would walk towards targets
+ * <p>Definition of "have brains":
+ * <ul>
+ * <li>Would wander around</li>
+ * <li>Would look for targets</li>
+ * <li>Would walk towards targets</li>
+ * </ul>
+ * </p>
  */
 public abstract class Living extends Entity {
 
-    public final static String MODIFIER_JUMP = "net.blockserver.entity.Living.jump",
-            MODIFIER_FALL = "net.blockserver.entity.Living.fall",
-            MODIFIER_PITCH_NEUTRALIZER = "net.blockserver.entity.Living.neutralizer.pitch",
-            MODIFIER_WATER= "net.blockserver.entity.Living.water";
+    /**
+     * The modifier that increases an entity's vertical component when it jumps.
+     */
+    public final static String MODIFIER_JUMP = "net.blockserver.entity.Living.jump";
+    /**
+     * The modifier that decreases an entity's vertical component when it falls.
+     */
+    public final static String MODIFIER_FALL = "net.blockserver.entity.Living.fall";
+    /**
+     * The modifier that affects an entity's motion when it is in contact with flowing water.
+     */
+    public final static String MODIFIER_WATER= "net.blockserver.entity.Living.water";
 
     public final static double WANDER_PERCENTAGE = 0.03;
 
