975aadc4be81066663818e21bd8da13f42284e91
PEMapModder <pemapmodder1970@gmail.com>
2014-09-14 16:43:48 +0800

    MessagePacket upper limit should be 0xFFFF

--- src/java/org/blockserver/network/minecraft/MessagePacket.java
+++ src/java/org/blockserver/network/minecraft/MessagePacket.java
@@ -7,15 +7,12 @@ public class MessagePacket implements BaseDataPacket{
 	private ByteBuffer bb;
 	private String msg;
 	public MessagePacket(String message){
-		if(message.length() > 0xFF){
-			throw new RuntimeException("Message '" + message +
-					"' was passed to MessagePacket, but the message length exceeds "
-					+ "the maximum length (255) by " + Integer.toString(message.length() - 255) 
-					+ " character(s).");
+		if(message.length() > 0xFFFF){
+			throw new StringIndexOutOfBoundsException(String.format("Message '%s' passed to MessagePacket, exceeding the maximum length (%d) by %d character(s)", message, 0xFFFF, message.length() - 0xFFFF));
 //			Server.getInstance().getLogger().warning("Message '%s' was passed to MessagePacket, "
 //					+ "but the message length exceeds the maximum length %d by %d character(s).",
-//					message, 0xFF, message.length() - 0xFF);
-//			message = message.substring(0, 0xFF);
+//					message, 0xFFFF, message.length() - 0xFFFF);
+//			message = message.substring(0, 0xFFFF);
 		}
 		msg = message;
 	}
