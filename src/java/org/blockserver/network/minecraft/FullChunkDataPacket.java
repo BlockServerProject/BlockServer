d004ae52c6470a64fb1a1578d0bd8b9ce11377b3
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-18 01:24:08 +0900

    Change FullChunkDataPacket
    Import DarkJET

--- src/java/org/blockserver/network/minecraft/FullChunkDataPacket.java
+++ src/java/org/blockserver/network/minecraft/FullChunkDataPacket.java
@@ -1,39 +1,39 @@
 package org.blockserver.network.minecraft;
 
 import java.nio.ByteBuffer;
-import java.util.zip.Deflater;
-
 import org.blockserver.level.provider.IChunk;
 import org.blockserver.utility.Utils;
 
 public class FullChunkDataPacket extends BaseDataPacket{
 	private IChunk chunk;
 	private byte[] buffer;
-
+	
+	public final static int UNCOMPRESSED_CHUNK_LENGTH = (Integer.BYTES * 2) + 0x8000 + (0x4000*3) + 0x100 + 0x400;
+	public static final byte[] BIOMECOLOR = new byte[]{0x00 ,(byte) 0x85 ,(byte) 0xb2 ,0x4a};
+	
 	public FullChunkDataPacket(IChunk chunk){
 		this.chunk = chunk;
 	}
 
 	@Override
 	public void encode(){
-		ByteBuffer bb = ByteBuffer.allocate(0x20000);
-		bb.put(Utils.putLTriad(chunk.getX()));
-		bb.put(Utils.putLTriad(chunk.getZ()));
-		bb.put(chunk.getBlocks());
-		bb.put(chunk.getDamages());
-		bb.put(chunk.getSkyLights());
-		bb.put(chunk.getBlockLights());
-		bb.put(chunk.getTiles());
-
-		Deflater deflater = new Deflater(7);
-		deflater.setInput(bb.array());
-		deflater.finish();
-		buffer = new byte[(int) deflater.getBytesWritten()];
-		int length = deflater.deflate(buffer);
-		if(length == 0){
-			throw new RuntimeException("Length unexpectedly 0");
+		//TODO add Tiles to Chunk Packet
+		//TODO handle BIOMECOLOR or BIOMEIDs
+		ByteBuffer afterBuffer = ByteBuffer.allocate(0x100 + 0x400);
+		int last = 0x100;
+		while( afterBuffer.position() != last ) {
+			afterBuffer.put((byte) 0xff);
 		}
-		deflater.end();
+		last += 0x400;
+		while( afterBuffer.position() != last ) {
+			afterBuffer.put(BIOMECOLOR);
+		}
+		try{
+			buffer = Utils.compressByte( Utils.putLTriad(chunk.getX()), Utils.putLTriad(chunk.getZ()), chunk.getBlocks(), chunk.getDamages(), chunk.getSkyLights(), chunk.getBlockLights(), afterBuffer.array() );
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	@Override
 	public void decode(){
