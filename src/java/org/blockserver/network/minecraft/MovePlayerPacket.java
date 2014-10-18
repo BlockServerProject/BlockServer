a222f2f14a91a9f23d31727ed33d711483bb9b98
Blue Electric <fkdlxmslrx@gmail.com>
2014-10-18 10:29:28 +0900

    Add MovePlayerPacket
    Import of
    https://github.com/bluelectric/DarkJETServer/commit/f58c22c16a5eab3179824e595eebf40a75d9dc4c
    
    이동! 돌진하라

---
+++ src/java/org/blockserver/network/minecraft/MovePlayerPacket.java
@@ -0,0 +1,41 @@
+package org.blockserver.network.minecraft;
+
+import java.nio.ByteBuffer;
+
+public final class MovePlayerPacket extends BaseDataPacket{
+	public int eid;
+	public float x;
+	public float y;
+	public float z;
+	public float yaw;
+	public float pitch;
+	public float bodyYaw;
+	public boolean isTeleport = false;
+
+	@Override
+	public void decode() {
+		bb.get();
+		eid = bb.getInt();
+		x = bb.getFloat();
+		y = bb.getFloat();
+		z = bb.getFloat();
+		yaw = bb.getFloat();
+		pitch = bb.getFloat();
+		bodyYaw = bb.getFloat();
+	}
+
+	@Override
+	public void encode() {
+		bb = ByteBuffer.allocate( 2 + Integer.BYTES + (Float.BYTES * 6) );
+		bb.put(MOVE_PLAYER);
+		bb.putInt(eid);
+		bb.putFloat(x);
+		bb.putFloat(y);
+		bb.putFloat(z);
+		bb.putFloat(yaw);
+		bb.putFloat(pitch);
+		bb.putFloat(bodyYaw);
+		bb.put((byte) (isTeleport == true ? 0x80 : 0x00));
+	}
+	
+}
