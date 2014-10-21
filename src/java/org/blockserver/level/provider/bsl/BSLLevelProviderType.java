7cb9cca67a13d4155415f6b4848a265502bccaa0
PEMapModder <pemapmodder1970@gmail.com>
2014-10-21 18:30:52 +0800

    Add filter for BSL

--- src/java/org/blockserver/level/provider/bsl/BSLLevelProviderType.java
+++ src/java/org/blockserver/level/provider/bsl/BSLLevelProviderType.java
@@ -12,8 +12,13 @@ public class BSLLevelProviderType extends LevelProviderType<BSLLevelProvider>{
 	}
 	@Override
 	public boolean supports(File worldDir){
-		// TODO Auto-generated method stub
-		return false;
+		if(!worldDir.exists()){
+			return true;
+		}
+		if(worldDir.isFile()){
+			return false;
+		}
+		return new File(worldDir, "index.bsf").isFile();
 	}
 	@Override
 	public BSLLevelProvider instantiate(Server server, File worldDir, String name){
