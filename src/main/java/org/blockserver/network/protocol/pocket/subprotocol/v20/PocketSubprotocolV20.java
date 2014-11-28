f7287c178d0636f0f44277b767eaff2eb41c1795
PEMapModder <pemapmodder1970@gmail.com>
2014-11-28 20:15:57 +0800

    Rename package v0_10_0 to v20 as that is the actual protocol version

--- src/main/java/org/blockserver/network/protocol/pocket/subprotocol/v20/PocketSubprotocolV20.java
+++ src/main/java/org/blockserver/network/protocol/pocket/subprotocol/v20/PocketSubprotocolV20.java
@@ -1,13 +1,12 @@
-package org.blockserver.network.protocol.pocket.subprotocol.v0_10_0;
+package org.blockserver.network.protocol.pocket.subprotocol.v20;
 
 import org.blockserver.Server;
-import org.blockserver.network.protocol.pocket.raknet.RaknetReceivedCustomPacket.ReceivedEncapsulatedPacket;
 import org.blockserver.network.protocol.pocket.subprotocol.PocketDataPacket;
 import org.blockserver.network.protocol.pocket.subprotocol.PocketSubprotocol;
 
-public class PocketSubprotocol_0_10_0 extends PocketSubprotocol{
+public class PocketSubprotocolV20 extends PocketSubprotocol{
 	private Server server;
-	public PocketSubprotocol_0_10_0(Server server){
+	public PocketSubprotocolV20(Server server){
 		this.server = server;
 	}
 	@Override
