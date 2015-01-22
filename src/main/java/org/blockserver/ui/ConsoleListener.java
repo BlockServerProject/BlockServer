a591de0f545d066b8ecfe6ddccc24e29dde24ace
SuperstarGamer <trent0308@live.com>
2015-01-21 19:00:20 -0600

    Added/Fixed Command Arguments
    Edited ConsoleListener Class to retriever the arguments without the
    command itselft in side of it. For example, if the console runs kick
    SuperstarGamer HAXTOTHEMAX, it will no longer show "kick" as the first
    argument for the command instead of "SuperstarGamer"

--- src/main/java/org/blockserver/ui/ConsoleListener.java
+++ src/main/java/org/blockserver/ui/ConsoleListener.java
@@ -1,5 +1,8 @@
 package org.blockserver.ui;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.blockserver.command.Command;
 import org.blockserver.command.CommandMessage;
 import org.blockserver.command.CommandType;
@@ -18,12 +21,23 @@ public class ConsoleListener{
 				return;
 			}
 			else {
-				String[] commandArgs = line.split(" ");
-				CommandType command = Command.getCommandType(commandArgs[0]);
+				// Get raw command args
+				String[] rawCommandArgs = line.split(" ");
+				// Get command
+				CommandType command = Command.getCommandType(rawCommandArgs[0]);
+				
+				// Remove command from arguments
+				ArrayList<String> argsArray = new ArrayList<String>(Arrays.asList(rawCommandArgs));
+				argsArray.remove(0);
+				
+				// Create arguments
+				String[] args = argsArray.toArray(new String[argsArray.size()]);
+				
 				if(command.equals(CommandType.HELP) || command.equals(CommandType.ALT_HELP)) {
 					System.out.println("This command is not yet finished!");
 				}
 				else {
+					System.out.println(args[0]);
 					System.out.println(CommandMessage.UnknownCommand);
 				}
 				// TODO add all commands
