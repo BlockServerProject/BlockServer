99233708b652a776003b50c1429c9505abab8b6f
jython234 <jython234@hotmail.com>
2015-02-13 17:37:33 -0600

    API change.

--- src/main/java/org/blockserver/api/Plugin.java
+++ src/main/java/org/blockserver/api/Plugin.java
@@ -1,13 +1,14 @@
 package org.blockserver.api;
 
 import org.blockserver.Server;
+import org.blockserver.ui.Logger;
 
 /**
  * Represents a Plugin.
  */
 public interface Plugin {
 
-    void onLoad(Server server);
+    void onLoad(Server server, Logger logger);
 
     void onEnable();
     void onDisable();
