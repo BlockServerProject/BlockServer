97e6ff8ffc3ae69b86c1ed1ddb79469351fddf09
jython234 <jython234@hotmail.com>
2015-02-14 16:29:13 -0600

    Uploaded TCPBridge Implementation.

---
+++ src/main/java/org/blockserver/net/bridge/tcp/TCPBridge.java
@@ -0,0 +1,59 @@
+package org.blockserver.net.bridge.tcp;
+
+import org.blockserver.Server;
+import org.blockserver.net.bridge.NetworkBridge;
+import org.blockserver.net.bridge.NetworkBridgeManager;
+import org.blockserver.net.protocol.WrappedPacket;
+
+import java.lang.reflect.Constructor;
+import java.net.SocketAddress;
+
+/**
+ * TCP/IP Network Bridge.
+ */
+public class TCPBridge extends NetworkBridge{
+    private TCPSocket sock;
+    private NetworkBridgeManager mgr;
+
+    public TCPBridge(NetworkBridgeManager mgr, SocketAddress bindAddr, Class<? extends TCPSocket> sockImpl){
+        this.mgr = mgr;
+        try {
+            Constructor construct = sockImpl.getConstructor(Server.class, SocketAddress.class);
+            sock = (TCPSocket) construct.newInstance(mgr.getServer(), bindAddr);
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        } finally {
+            start();
+        }
+    }
+
+    private void start(){
+        sock.setRunning(true);
+        sock.start();
+    }
+
+    @Override
+    public boolean send(byte[] buffer, SocketAddress addr) {
+        return sock.send(buffer, addr);
+    }
+
+    @Override
+    public WrappedPacket receive() {
+        WrappedPacket wp = sock.receive();
+        if(wp != null){
+            return wp;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Server getServer() {
+        return mgr.getServer();
+    }
+
+    @Override
+    public String getDescription() {
+        return "A NetworkBridge for TCP/IP.";
+    }
+}
---
+++ src/main/java/org/blockserver/net/bridge/tcp/TCPSocket.java
@@ -0,0 +1,41 @@
+package org.blockserver.net.bridge.tcp;
+
+import org.blockserver.Server;
+import org.blockserver.net.protocol.WrappedPacket;
+
+import java.net.SocketAddress;
+
+/**
+ * TCP/IP Socket. Must be extended from all implementations.
+ */
+public abstract class TCPSocket extends Thread{
+    private Server server;
+    private SocketAddress bindAddr;
+    private boolean running = false;
+
+    public TCPSocket(Server server, SocketAddress bindAddr){
+        this.server = server;
+        this.bindAddr = bindAddr;
+    }
+
+    public abstract WrappedPacket receive();
+    public abstract boolean send(byte[] buffer, SocketAddress sendTo);
+
+    public boolean send(WrappedPacket wp){
+        return send(wp.bb().array(), wp.getAddress());
+    }
+
+    public void setRunning(boolean running){
+        this.running = running;
+    }
+
+    public boolean isRunning() { return running; }
+
+    public Server getServer(){
+        return server;
+    }
+
+    public SocketAddress getAddress(){
+        return bindAddr;
+    }
+}
