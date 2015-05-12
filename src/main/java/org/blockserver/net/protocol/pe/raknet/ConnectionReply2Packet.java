1d876d44609e32d30c3bda30745555d52a45ab49
jython234 <jython234@hotmail.com>
2015-05-11 20:05:15 -0500

    Finished ConnectionReply2.

--- src/main/java/org/blockserver/net/protocol/pe/raknet/ConnectionReply2Packet.java
+++ src/main/java/org/blockserver/net/protocol/pe/raknet/ConnectionReply2Packet.java
@@ -2,6 +2,8 @@ package org.blockserver.net.protocol.pe.raknet;
 
 import org.blockserver.net.protocol.pe.PeProtocolConst;
 
+import java.nio.ByteBuffer;
+
 /**
  * ID_OPEN_CONNECTION_REPLY_2 (0x08)
  */
@@ -10,7 +12,17 @@ public class ConnectionReply2Packet extends RakNetPacket{
     public short clientUdpPort;
     public short mtuSize;
     public byte security = 0;
-    
+
+    @Override
+    protected void _encode(ByteBuffer bb) {
+        bb.put(PeProtocolConst.RAKNET_OPEN_CONNECTION_REPLY_2);
+        bb.put(PeProtocolConst.MAGIC);
+        bb.putLong(serverID);
+        bb.putShort(clientUdpPort);
+        bb.putShort(mtuSize);
+        bb.put(security);
+    }
+
     @Override
     public int getLength() {
         return 30;
