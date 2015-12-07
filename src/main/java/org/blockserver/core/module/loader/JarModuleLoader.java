d3a83c5a0e31d18bccb2dcc069e0fa01fd63a386
jython234 <jython234@gmail.com>
2015-12-07 17:55:40 -0600

    Begin JAR Module loader.

---
+++ src/main/java/org/blockserver/core/module/loader/JarModuleLoader.java
@@ -0,0 +1,38 @@
+package org.blockserver.core.module.loader;
+
+
+import org.blockserver.core.Server;
+import org.blockserver.core.module.Module;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.jar.JarFile;
+
+/**
+ * Module Loader that can load modules from JARs
+ */
+public class JarModuleLoader implements ModuleLoader{
+    @Override
+    public Collection<Module> setModules(Collection<Module> currentModules, Server server) {
+        File moduleFolder = new File("modules");
+        if(!moduleFolder.isDirectory()) {
+            moduleFolder.mkdirs();
+            return currentModules;
+        }
+        File[] files = moduleFolder.listFiles();
+        if(files == null && files.length <= 0) {
+            return currentModules;
+        }
+        for(File file : files) {
+            if(file.getName().endsWith(".jar")) {
+                try {
+                    JarFile jar = new JarFile(file);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
